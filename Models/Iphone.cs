2ce28076e5c76aba3907b9560c8feacb5efa57ec
PamellaGoncalves <pamellapag01@gmail.com>
2024-01-11 16:14:23 -0300

    desafio-poo

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
+
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        Console.WriteLine("Abrindo o App Store...");
+        Console.WriteLine($"{nomeApp} está sendo instalado");
+        Console.WriteLine("...");
+        Console.WriteLine("Instalado com sucesso!");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,6 +3,13 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Nokia
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+     public Nokia(string numero, string modelo, string imei, string memoria) : base(numero, modelo, imei, memoria)
     }
+     public override void InstalarAplicativo(string nomeApp) {
+            Console.WriteLine("Abrindo Play Store...");
+            Console.WriteLine($"{nomeApp} está sendo instalado");
+            Console.WriteLine("...");
+            Console.WriteLine("Instalado com sucesso!");
+     }
 }
+
